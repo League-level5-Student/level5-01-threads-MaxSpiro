e865dccf5ec27a1c7fe037a6c7c7de44ee4a029d
MaxSpiro <49620236+MaxSpiro@users.noreply.github.com>
2020-05-17 13:41:45 -0700

    finished module 1

--- src/_03_Threaded_Reverse_Greeting/ThreadedReverseGreeting2.java
+++ src/_03_Threaded_Reverse_Greeting/ThreadedReverseGreeting2.java
@@ -21,19 +21,11 @@ public class ThreadedReverseGreeting2 {
 	//1. Create a new class called ThreadedGreeter that implements the Runnable interface.
 	//   Be sure to implement the run() method.
 	
-	//2. Give the ThreadedGreeter class a  member variable of the integer type and a constructor
-	//   to initialize the member variable to a custom value.
-	
-	//3. In the run method of the ThreadedGreeter class, print the message using the member variable as the thread number.
-	//   If the member integer is less than or equal to 50, create a new thread. 
-	//   Pass in a new object of the ThreadedGreeter class with the value of the member variable plus one.
-	
-	//4. Start the thread and join it with the calling thread.
-	
 	public static void main(String[] args) {
 		//5. Create the first thread and initialize it with an object of the ThreadedGreeter class.
 		//   The ThreadedGreeter object should be initialized with 1.
-		Thread t = new Thread(new ThreadedGreeter(1));
+		ThreadedGreeter tg = new ThreadedGreeter(1);
+		Thread t = new Thread(tg);
 		
 		//6. Start and join the thread. Did you get the required output?
 		t.start();
@@ -44,3 +36,4 @@ public class ThreadedReverseGreeting2 {
 		}
 	}
 }
+
--- src/_05_Synchronized_Swimming/Swimmer.java
+++ src/_05_Synchronized_Swimming/Swimmer.java
@@ -16,6 +16,9 @@ public class Swimmer extends Thread {
 
 	@Override
 	public void run() {
-		// ...
+		for(int i=0;i<5;i++) {
+			new SynchronizedSwimming();
+			SynchronizedSwimming.takeTurn(new Swimmer(name));
+		}
 	}
 }
